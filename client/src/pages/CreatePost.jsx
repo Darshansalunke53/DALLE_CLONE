d69f8601edc180dbda87ad874aa9d9b688c7aac7
Darshansalunke53 <darshansalunke53@gmail.com>
2023-07-11 03:29:38 +0530

    Add style to the image box

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -107,7 +107,7 @@ const CreatePost = () => {
                         handleSurpriseMe={handleSurpriseMe}
                     />
 
-                    <div>
+                    <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
                         {form.photo ? (
                             <img
                                 src={form.photo}
@@ -118,7 +118,7 @@ const CreatePost = () => {
                             <img
                                 src={preview}
                                 alt="preview"
-                                className="w-4/12 h-4/12 object-contain opacity-40"
+                                className="w-9/12 h-9/12 object-contain opacity-40"
                             />
                         )}
                         {generatingImg && (
